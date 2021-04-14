443b2d4bf21fe79c2cf356debb70f1cdb494f4f7
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-14 09:48:06 +0530

    Start with one player at O position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,7 @@
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game");
+        final int INITIAL_POSITION=0;
+        final int NO_OF_PLAYERS=1;
+    }
+}
