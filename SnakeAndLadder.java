11587b05e964ae5902bc8debd88da7b2d254c615
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-14 11:07:05 +0530

    Repeat till the player reaches winnig position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,29 +4,35 @@ public class SnakeAndLadder{
 	public static final int noPlay=1;
 	public static final int Ladder=2;
 	public static final int Snake=3;
+	public static final int minPosition=0;
+
 	public static void main(String[] args){
 
 		int playerPosition=startPosition;
-
 		System.out.println("Player at Start Position "+startPosition);
-		int diceRoll=((int)Math.floor(Math.random()*10)%6)+1;
-		System.out.println("Dice Roll : "+diceRoll);
+		while(playerPosition<100){ //player reaches the winning position 100
 
-		int optionCheck=(int)Math.floor(Math.random()*10)%3+1;
-		switch(optionCheck){
-			case Ladder:
-				System.out.println("Ladder");
-				playerPosition+=diceRoll;
-				break;
-			case Snake:
-				System.out.println("Snake");
-				playerPosition-=diceRoll;
-				break;
-			case noPlay:
-				System.out.println("No Play");
-				break;
-			default:
+			int diceRoll=((int)Math.floor(Math.random()*10)%6)+1;
+			System.out.println("Dice Roll : "+diceRoll);
+			int optionCheck=(int)Math.floor(Math.random()*10)%3+1;
+			switch(optionCheck){
+				case Ladder:
+					System.out.println("Ladder");
+					playerPosition+=diceRoll;
+					break;
+				case Snake:
+					System.out.println("Snake");
+					playerPosition=(playerPosition-diceRoll)>=minPosition?(playerPosition-diceRoll):minPosition;
+					if(playerPosition==0)
+					System.out.println("Restarts from 0 position");
+					break;
+				case noPlay:
+					System.out.println("No Play");
+					break;
+				default:
+			}
+			System.out.println("Player Position : "+playerPosition);
 		}
-		System.out.println("Player Position : "+playerPosition);
+
 	}
 }
